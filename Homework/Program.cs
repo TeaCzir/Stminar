f4ce20f9f173314e76d046cb9f8d5455d0a49c55
TeaCzir <128166070+TeaCzir@users.noreply.github.com>
2023-03-28 16:01:10 +0300

    Добавлено много файлов

---
+++ Homework/Program.cs
@@ -0,0 +1,9 @@
+﻿//Число в квадрате.
+Console.Clear();
+Console.Write("Введите число: ");
+int number;
+number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа{number} равен {sqr}");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа{number} равен {sqr1}");
---
+++ Homework01/Program.cs
@@ -0,0 +1,7 @@
+﻿//проверяем.
+Console.Write("Введите число:");
+int number;
+number = int.Parse(Console.ReadLine());
+Console.WriteLine(number);
+int result = number * number;
+Console.WriteLine(result);
